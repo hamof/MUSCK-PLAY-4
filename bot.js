a28c13f2762443f73a25d0fd43e227b219d08f00
hamof <40973927+hamof@users.noreply.github.com>
2018-09-01 08:19:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -244,18 +244,18 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 });
 
 client.on("message", message => {
- if (message.content === `${prefix}`) {
+ if (message.content === `${prefix}help`) {
   const embed = new Discord.RichEmbed() //by ,$ ReBeL ء , ??#4777 'CODES SERVER'
       .setColor("#000000")//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
       .setDescription(`
-${prefix}play ? لتشغيل أغنية برآبط أو بأسم
-${prefix}skip ? لتجآوز الأغنية الحآلية
-${prefix}pause ? إيقآف الأغنية مؤقتا
-${prefix}resume ? لموآصلة الإغنية بعد إيقآفهآ مؤقتا
-${prefix}vol ? لتغيير درجة الصوت 100 - 0
-${prefix}stop ? لإخرآج البوت من الروم
-${prefix}np ? لمعرفة الأغنية المشغلة حآليا
-${prefix}queue ? لمعرفة قآئمة التشغيل
+${prefix}play  لتشغيل أغنية برآبط أو بأسم
+${prefix}skip  لتجآوز الأغنية الحآلية
+${prefix}pause  إيقآف الأغنية مؤقتا
+${prefix}resume  لموآصلة الإغنية بعد إيقآفهآ مؤقتا
+${prefix}vol  لتغيير درجة الصوت 100 - 0
+${prefix}stop  لإخرآج البوت من الروم
+${prefix}np  لمعرفة الأغنية المشغلة حآليا
+${prefix}queue  لمعرفة قآئمة التشغيل
 
  `)//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
    message.channel.sendEmbed(embed)//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
